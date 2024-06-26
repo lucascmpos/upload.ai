6d9d7eea31112fcaa8c2a4264e9f7bae7d6a710d
lucascmpos <lucasoliveiracampos81@gmail.com>
2024-06-26 13:03:55 -0300

    fix: fixing deploy

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ export function App() {
     completion,
     isLoading,
   } = useCompletion({
-    api: "http://localhost:3333/ai/complete",
+    api: "https://upload-aibackend.vercel.app/ai/complete",
     body: {
       videoId,
       temperatura,
